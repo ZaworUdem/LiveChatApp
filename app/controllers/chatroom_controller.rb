76dec0a75a41c770eb075918b12be2d1538031a6
Piotr Zaworski <fuhorte@gmail.com>
2019-08-01 11:54:37 +0200

    Live messaging, channels, some JS, end of section in course

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -2,8 +2,8 @@ class ChatroomController < ApplicationController
     before_action :require_user
 
     def index 
-        @messages = Message.all
+        @messages = Message.custom_display
         @message = Message.new
     end
-    
+
 end
