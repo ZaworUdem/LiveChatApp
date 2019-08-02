fa87fd592c188e2bae45f02aa8442fc75b67559a
Piotr Zaworski <fuhorte@gmail.com>
2019-08-02 12:04:59 +0200

    Added sign up functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,34 @@
 class UsersController < ApplicationController
 
-    def new
+    before_action :logged_in_redirect, only: [:new, :create]
 
+    def new
+        @user = User.new
     end
 
     def create 
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            flash[:success] = "You have succesfully signed up!"
+            redirect_to root_path
+        else
+            render 'new'
+            flash[:error] = "Something went wrong! :("
+        end
+    end
 
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+
+    def logged_in_redirect
+        if logged_in?
+            flash[:error] = "You are already logged in"
+            redirect_to root_path
+        end
     end
 
 end
